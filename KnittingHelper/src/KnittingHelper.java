bef169303279ba583ae9b14fb751556bfcb988cd
rachelbkeenan <rachelbkeenan@gmail.com>
2025-04-08 18:38:01 -0400

    Added toFlat() method functionality

--- KnittingHelper/src/KnittingHelper.java
+++ KnittingHelper/src/KnittingHelper.java
@@ -63,8 +63,42 @@ public class KnittingHelper {
 			
 		}
 		else if((s.compareTo("B") == 0) || (s.compareTo("b") == 0)) {
-			System.out.println("B");
-			ToFlat.toFlat();
+			
+			System.out.println("Enter the number of cast on stitches in the original pattern: ");
+			int col = scan.nextInt();
+			
+			while(col <= 0) {
+				System.out.println("Please enter a value greater than zero: ");
+				col = scan.nextInt();
+			}
+			
+			System.out.println("Enter the number of rows that repeat in the original pattern: ");
+			int row = scan.nextInt();
+			
+			while(row <= 0) {
+				System.out.println("Please enter a value greater than zero: ");
+				row = scan.nextInt();
+			}
+			
+			String[][] matrix = new String[row][col];
+			
+			System.out.println("Enter your pattern with spaces between each stitch type: ");
+			
+			for(int i = 0; i < row; i++) {
+				for(int j = 0; j < col; j++) {
+					matrix[i][j] = scan.next();
+				}
+			}
+			
+			
+//			for(int i = 0; i < row; i++) {
+//				for(int j = 0; j < col; j++) {
+//					System.out.print(matrix[i][j]);
+//				}
+//				System.out.println();
+//			}
+//			
+			ToFlat.toFlat(row, col, matrix);
 		}
 		else if((s.compareTo("C") == 0) || (s.compareTo("c") == 0)) {
 			System.out.println("C");
--- KnittingHelper/src/ToFlat.java
+++ KnittingHelper/src/ToFlat.java
@@ -3,21 +3,88 @@
  * Date: 4/7/2025
  * Info: Method to change pattern from "in the round" to "flat"
  * Based on: https://nimble-needles.com/tutorials/how-to-convert-a-knitting-pattern-from-in-the-round-to-flat-knitting/
- * 
+ * Notes: Should add more user-friendly instructions, like the different stitch types and their acronyms
  */
 
 public class ToFlat {
 
-	public static void toFlat(){
+	public static void toFlat(int rows, int cols, String[][] ogPattern){
 		
-		String[][] matrix = {{"a", "b","c"}, {"d", "e", "f"}};
-		//will look like
-		//String[][] matrix = new String[r][c];
+		String[][] newPattern = new String[rows][cols+2];
 		
 		
+		//adding knits to the boarder for the seam of the garment
+		for(int i = 0; i < rows; i++) {
+			newPattern[i][0] = "k";
+			newPattern[i][cols+1] = "k";
+		}
+		
+		//converting the pattern
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j < cols; j++) {
+				
+				
+				if(i%2 == 0) {								//the "right side" of the pattern
+					newPattern[i][j+1] = ogPattern[i][j];
+				}
+				else {										//the "wrong side" of the pattern
+					String stitch = ogPattern[i][cols-j-1]; //for simplicity
+					
+					if(stitch.compareTo("k") == 0) {
+						newPattern[i][j+1] = "p";
+					}
+					else if(stitch.compareTo("p") == 0) {
+						newPattern[i][j+1] = "k";
+					}
+					else if(stitch.compareTo("ktbl") == 0) {
+						newPattern[i][j+1] = "ptbl";
+					}
+					else if(stitch.compareTo("ptbl") == 0) {
+						newPattern[i][j+1] = "ktbl";
+					}
+					else if(stitch.compareTo("k2tog") == 0) {
+						newPattern[i][j+1] = "p2tog";
+					}
+					else if(stitch.compareTo("p2tog") == 0) {
+						newPattern[i][j+1] = "k2tog";
+					}
+					else if(stitch.compareTo("ssk") == 0) {
+						newPattern[i][j+1] = "ssp";
+					}
+					else if(stitch.compareTo("ssp") == 0) {
+						newPattern[i][j+1] = "ssk";
+					}
+					else if(stitch.compareTo("m1r") == 0) {
+						newPattern[i][j+1] = "m1pr";
+					}
+					else if(stitch.compareTo("m1pr") == 0) {
+						newPattern[i][j+1] = "m1r";
+					}
+					else if(stitch.compareTo("m1l") == 0) {
+						newPattern[i][j+1] = "m1pl";
+					}
+					else if(stitch.compareTo("m1pl") == 0) {
+						newPattern[i][j+1] = "m1l";
+					}
+					else {
+						newPattern[i][j+1] = "0";
+					}
+				}
+				
+			}
+		}
 		
 		
-		System.out.println("to flat");
+		System.out.println("Your new pattern is: ");
+		
+		//print new pattern
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j < cols+2; j++) {
+				System.out.print(newPattern[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
 	}
 	
 }
